a5158aea65e65e13259c7d160d5e132bdb223f52
llmthpanll <methasitpan@gmail.com>
2022-09-02 02:15:12 +0700

    fail

---
+++ lab4/3/javascript.js
@@ -0,0 +1,27 @@
+const std_number = document.getElementById('std_number');
+const std_name = document.getElementById('std_name');
+const surname = document.getElementById('surname');
+const insertHere = document.getElementById('insert');
+const std_btn = document.getElementById('btn_submit');
+
+let order = 0;
+
+function regis() {
+    order++;
+    console.log("soduhpfuio");
+    let newTr = document.createElement('tr');
+    let newTd1 = document.createElement('td');
+    let newTd2 = document.createElement('td');
+    let newTd3 = document.createElement('td');
+    let newTd4 = document.createElement('td');
+    newTd1.innerHTML = order;
+    newTd2.innerHTML = std_number.value;
+    newTd3.innerHTML = std_name.value;
+    newTd4.innerHTML = surname.value;
+    newTr.appendChild(newTd1);
+    newTr.appendChild(newTd2);
+    newTr.appendChild(newTd3);
+    newTr.appendChild(newTd4);
+    insertHere.appendChild(newTr);
+
+}
