5611e0da18034ffc00658f7d6e237814b68c2c38
llmthpanll <methasitpan@gmail.com>
2022-09-04 17:23:51 +0700

    FINISHED LAB4/3

--- lab4/3/javascript.js
+++ lab4/3/javascript.js
@@ -1,27 +1,26 @@
-const std_number = document.getElementById('std_number');
-const std_name = document.getElementById('std_name');
-const surname = document.getElementById('surname');
-const insertHere = document.getElementById('insert');
-const std_btn = document.getElementById('btn_submit');
-
-let order = 0;
-
+let seq = 1;
 function regis() {
-    order++;
-    console.log("soduhpfuio");
-    let newTr = document.createElement('tr');
-    let newTd1 = document.createElement('td');
-    let newTd2 = document.createElement('td');
-    let newTd3 = document.createElement('td');
-    let newTd4 = document.createElement('td');
-    newTd1.innerHTML = order;
-    newTd2.innerHTML = std_number.value;
-    newTd3.innerHTML = std_name.value;
-    newTd4.innerHTML = surname.value;
-    newTr.appendChild(newTd1);
-    newTr.appendChild(newTd2);
-    newTr.appendChild(newTd3);
-    newTr.appendChild(newTd4);
-    insertHere.appendChild(newTr);
-
+    let std_id = document.getElementById('std_id').value;
+    let std_name = document.getElementById('std_name').value;
+    let std_surname = document.getElementById('std_surname').value;
+    let insertHere = document.getElementById('insert');
+    let row = document.createElement("tr");
+    let col1 = document.createElement("td");
+    let col2 = document.createElement("td");
+    let col3 = document.createElement("td");
+    let col4 = document.createElement("td");
+    let text1 = document.createTextNode(seq);
+    let text2 = document.createTextNode(std_id);
+    let text3 = document.createTextNode(std_name);
+    let text4 = document.createTextNode(std_surname);
+    col1.appendChild(text1);
+    col2.appendChild(text2);
+    col3.appendChild(text3);
+    col4.appendChild(text4);
+    row.appendChild(col1);
+    row.appendChild(col2);
+    row.appendChild(col3);
+    row.appendChild(col4);
+    insertHere.appendChild(row);
+    seq++
 }
