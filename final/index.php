ce54f3728dfc660715597044943a975c6f50491b
Methasit Prateepmaethakul <54169574+llmthpanll@users.noreply.github.com>
2022-12-06 00:07:23 +0700

    .

--- final/index.php
+++ final/index.php
@@ -1,47 +1,18 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Web Tech Final Example</title>
-    <style>
-        #map {
-            height: 400px;
-            width: 90%;
-        }
-    </style>
+    <title>Document</title>
 </head>
+
 <body>
-    <?php    
-        
-        $url = "data.json";
-        $response = file_get_contents($url);
-        $result = json_decode($response);
-        echo "<form action='index.php' method='post'>
-        <div>
-            <select name='location' id='location'>
-                <option value='' disabled selected>เลือกชื่อสถานที่</option>";
-        foreach ($result as $place) {
-            echo "<option value='$place->name'>$place->name</option>"; 
-        }
-        echo "</select>";
-        echo "
-            </div>
-            <button type='submit' id='submit'>Submit</button>
-        </form>";
-        if (isset($_POST['location'])){
-            if (!empty($_POST['location'])){
-                $selected = $_POST['location'];
-                echo '<iframe id="map" width="100%" height="500" src="https://maps.google.com/maps?q='.$selected.'&t=&z=5&ie=UTF8&iwloc=&output=embed"
-                    frameborder="0" allowfullscreen>
-                </iframe>';
-            }
-            else {
-                echo '<iframe id="map" width="100%" height="500" src="https://maps.google.com/maps?q=&t=&z=5&ie=UTF8&iwloc=&output=embed"
-                    frameborder="0" allowfullscreen>
-                </iframe>';
-            }
-        }
-    ?>
+    <form action="" method="POST">
+        <label for="prodname">Name :</label>
+        <input type="text" id="prodname" name="prodname" placeholder="Enter Product Name" required />
+        <button type="submit" name="submit">Submit</button>
+    </form>
 </body>
+
 </html>
